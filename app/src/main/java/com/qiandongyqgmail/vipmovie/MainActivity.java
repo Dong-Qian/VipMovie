82e3b528ec54233479fbd9eb09d47a6d808c41eb
Dong-Qian <qiandongyq@gmail.com>
2017-01-17 22:39:03 -0500

    changed the movie cell layout. Need to add Decoration  to main recyclerView

--- app/src/main/java/com/qiandongyqgmail/vipmovie/MainActivity.java
+++ app/src/main/java/com/qiandongyqgmail/vipmovie/MainActivity.java
@@ -6,10 +6,10 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,7 +18,6 @@ import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
-
 
     private Context context;
     private RecyclerView rv;
@@ -29,13 +28,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rv = (RecyclerView) findViewById(R.id.rv);
-        MovieAdapter adapter = new MovieAdapter(MainActivity .this);
-
+        MovieAdapter adapter = new MovieAdapter(MainActivity.this);
         rv.setLayoutManager(new LinearLayoutManager(this));
         rv.setAdapter(adapter);
 
-    }
+        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(rv.getContext(),
+                new LinearLayoutManager(this).getOrientation());
+        rv.addItemDecoration(mDividerItemDecoration);
 
+
+    }
 
 
     static class ViewHolder extends RecyclerView.ViewHolder {
@@ -72,8 +74,7 @@ public class MainActivity extends AppCompatActivity {
             LENGTH = ar.length();
             mAvator = new int[LENGTH];
 
-            for (int i = 0; i < LENGTH; i++)
-            {
+            for (int i = 0; i < LENGTH; i++) {
                 mAvator[i] = ar.getResourceId(i, 0);
             }
             ar.recycle();
@@ -82,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
         public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
 
-            return new ViewHolder(LayoutInflater.from(parent.getContext()),parent);
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
         }
 
         @Override
