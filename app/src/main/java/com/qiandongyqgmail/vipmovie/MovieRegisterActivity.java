e61f54211486b401be34d882cae4a4637aa6f3ac
Austin Che <acheese1234@gmail.com>
2017-01-27 00:39:54 -0500

    Updated AndroidManifest by adding the MovieRegisterActivity - allows for intent to proceed to "Register" screen.

--- app/src/main/java/com/qiandongyqgmail/vipmovie/MovieRegisterActivity.java
+++ app/src/main/java/com/qiandongyqgmail/vipmovie/MovieRegisterActivity.java
@@ -12,6 +12,8 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +24,7 @@ import android.widget.TextView;
 import java.lang.reflect.Array;
 
 public class MovieRegisterActivity extends AppCompatActivity
+    implements View.OnClickListener
 {
     /*-PRIVATE ATTRIBUTES-*/
     //-Widget specific fields-//
@@ -46,25 +49,52 @@ public class MovieRegisterActivity extends AppCompatActivity
 
         // Loading all the specified widget IDs
         movieTitle = (TextView) findViewById(R.id.TV_Register_Title);
+        etName = (EditText) findViewById(R.id.ET_Register_Name);
+        etDate = (EditText) findViewById(R.id.ET_Register_Date);
+        etTime = (EditText) findViewById(R.id.ET_Register_Time);
+        bttnDate = (Button) findViewById(R.id.Bttn_Register_Date);
+        bttnTime = (Button) findViewById(R.id.Bttn_Register_Time);
         ticketDropDown = (Spinner) findViewById(R.id.Spinner_Quantity);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.ticket_quantity, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
-        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         ticketDropDown.setAdapter(adapter);
 
 
         // From the MainActivity, get the position...
-        // We must access the resources to acquire the position?
         int mPosition = getIntent().getIntExtra("title", 0);
         Resources resources = getResources();
         title = resources.getStringArray(R.array.movie_title);
 
         movieTitle.setText(title[mPosition]);
-//        Log.i("Array Position", String.valueOf(mPosition));
+    }
+
+    /*
+     * METHOD : onClick
+     * DETAILS : The onClick method overrides the View onClick and
+     *      determines the widget that was clicked.
+     * PARAM :
+     * RETURN : None
+     */
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.Bttn_Register_Date:
+                // Call method to handle Date dialog
+                break;
+
+            case R.id.Bttn_Register_Time:
+                // Call method to handle Time dialog
+                break;
+
+            // Implement a "Buy/Purchase/Confirm" button
+            default:
+                break;
+        }
     }
 }
