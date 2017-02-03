41cf87138c71ed1edfb411144a7abc658a59188e
Austin Che <acheese1234@gmail.com>
2017-02-02 22:51:27 -0500

    Ticket class now calculates the price of a ticket - given the quantity and price. Now there is another version for calculatePrice that takes in two strings.

--- app/src/main/java/com/qiandongyqgmail/vipmovie/Ticket.java
+++ app/src/main/java/com/qiandongyqgmail/vipmovie/Ticket.java
@@ -1,24 +1,26 @@
 /*
-* FILE :            MovieRegisterActivity.java
+* FILE :            Ticket.java
 * PROJECT :         MAD - Assignment #1
 * PROGRAMMER :      Dong Qian (6573448) / Austin Che / Monira Sultana ()
 * FIRST VERSION :   2017-01-30
-* DESCRIPTION :
+* DESCRIPTION : The class definition and implementation for the Ticket class.
 */
 package com.qiandongyqgmail.vipmovie;
 
 
-import android.util.Log;
 
-/**
- * \class Ticket
- * \brief The Ticket class encapsulates the characteristics of a movie ticket. The Ticket has
+/*
+ * METHOD : Ticket
+ * DETAILS : The Ticket class encapsulates the characteristics of a movie ticket. The Ticket has
  *      attributes pertaining to the title of the movie, the purchaser's name, the date and time
  *      at which the movie plays, and many other attributes.
  */
 public class Ticket
 {
-    private String movieTitle;
+    private final float kIMAXPrice = 3.0f;
+    private final String kIMAXType = "IMAX";
+
+    private String movieTitle = null;
     private String buyerName = null;
     private String movieDate = null;
     private String movieTime = null;
@@ -26,18 +28,74 @@ public class Ticket
     private String ticketQuantity = null;
     private String totalPrice = null;
 
-    /**
-     * \brief Basic constructor for the Ticket class.
+
+    //==============================================
+    // CONSTRUCTORS
+    //----------------------------------------------
+    /*
+     * METHOD : Ticket
+     * DETAILS : Basic constructor.
      */
     public Ticket()
     {
         movieTitle = "N/A";
-        buyerName = "John Doe";
-        movieDate = "0000 - 00 - 00";
+        buyerName = "N/A";
+        movieDate = "1970 - 01 - 01";
         movieTime = "00 : 00 AM";
         movieType = "Regular";
         ticketQuantity = "0";
         totalPrice = "0.00";
+    }
+
+
+    //==============================================
+    // TICKET PRICE CALCULATIONS
+    //----------------------------------------------
+    /*
+     * METHOD : calculatePrice
+     * DETAILS : The ticket price is calculated by the product
+     *      of price and quantity plus extra fees. The extra fee
+     *      is dependent on the type of ticket (Regular, IMAX, etc.)
+     * PARAM : price - float - The price of the ticket
+     *         quantity - in - How many tickets are being bought
+     * RETURN : None
+     */
+    public void calculatePrice(float price, int quantity)
+    {
+        float total = 0;
+        float extra = 0;
+
+        if (movieType.equalsIgnoreCase(kIMAXType)) {
+            extra = kIMAXPrice;
+        }
+
+        total = (price * quantity) + extra;
+        setTotalPrice(total);
+    }
+
+
+    /*
+     * METHOD : calculatePrice
+     * DETAILS : The ticket price is calculated by the product
+     *      of price and quantity plus extra fees. The extra fee
+     *      is dependent on the type of ticket (Regular, IMAX, etc.)
+     * PARAM : price - String - The price of the ticket
+     *         quantity - String - How many tickets are being bought
+     * RETURN : None
+     */
+    public void calculatePrice(String price, String quantity)
+    {
+        float total = 0;
+        float extra = 0;
+
+        if (movieType.equalsIgnoreCase(kIMAXType)) {
+            extra = kIMAXPrice;
+        }
+
+        // Special case - need to convert String to Float
+        // for mathematical calculations
+        total = (Float.valueOf(price) * Float.valueOf(quantity)) + extra;
+        setTotalPrice(total);
     }
 
 
@@ -84,9 +142,9 @@ public class Ticket
         ticketQuantity = price;
     }
 
-    public void setTotalPrice(int price)
+    public void setTotalPrice(float price)
     {
-        totalPrice = String.valueOf(price);
+        totalPrice = String.format("%2.2f", price);
     }
 
 
